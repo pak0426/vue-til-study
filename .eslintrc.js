0f0e3eda5f7eda39c60facbe8228c7aa34e00835
hmmini <pak4184@naver.com>
2023-05-22 18:31:14 +0900

    - vue prettier 설정 및 호환성 버그 문제 해결

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:vue/vue3-essential', 'plugin:prettier/recommended'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['vue', 'prettier'],
+  rules: {
+    'no-console': 'off',
+    // "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    // "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: true,
+        useTabs: false,
+        tabWidth: 2,
+        trailingComma: 'all',
+        printWidth: 160,
+        bracketSpacing: true,
+        arrowParens: 'avoid',
+      },
+    ],
+  },
+};
