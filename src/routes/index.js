7a04ec8fc9f87fa6c6b87b29b73431eec9c787b4
hmmini <pak4184@naver.com>
2023-05-23 21:11:48 +0900

    - LoginPage, SignupPage 컴포넌트 생성

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,5 +4,14 @@ import VueRouter from 'vue-router';
 Vue.use(VueRouter);
 
 export default new VueRouter({
-  routes: [],
+  routes: [
+    {
+      path: '/login',
+      component: LoginPage,
+    },
+    {
+      path: '/signup',
+      component: SignupPage,
+    }
+  ],
 });
