774e37001ad99c9df0a133500558dcccbc6a1448
hmmini <pak4184@naver.com>
2023-05-23 21:01:51 +0900

    - Vue router 설치 및 인스턴스 생성

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
     // "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     // "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
     'prettier/prettier': [
-      'error',
+      'off',
       {
         singleQuote: true,
         semi: true,
