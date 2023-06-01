2c2b408e89c005f07f222f2f010b5ace85ec74c6
hmmini <pak4184@naver.com>
2023-06-01 20:11:34 +0900

    - 로그인 페이지 마크업 작성

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,23 @@
+<template>
+  <form>
+    <div>
+      <label for="email">id: </label>
+      <input id="email" type="text">
+    </div>
+    <div>
+      <label for="password">pw: </label>
+      <input id="password" type="text">
+    </div>
+    <button type="submit">로그인</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "LoginForm"
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
-    login
+    <h1>로그인 페이지</h1>
+    <LoginForm></LoginForm>
   </div>
 </template>
 
 <script>
+import LoginForm from '@/components/LoginForm.vue';
+
 export default {
-  name: "LoginPage"
+  components: {
+    LoginForm,
+  }
 };
 </script>
 
