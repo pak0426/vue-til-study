b334d45cad999a3beda9a30ca06a52df5ec5dcb2
hmmini <pak4184@naver.com>
2023-06-04 17:04:05 +0900

    - 로그인 폼 데이터 바인딩 및 이벤트 연결

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,12 +1,12 @@
 <template>
-  <form>
+  <form @submit.prevent="submitForm">
     <div>
       <label for="email">id: </label>
-      <input id="email" type="text">
+      <input id="email" type="text" v-model="email">
     </div>
     <div>
       <label for="password">pw: </label>
-      <input id="password" type="text">
+      <input id="password" type="text" v-model="password">
     </div>
     <button type="submit">로그인</button>
   </form>
@@ -14,7 +14,17 @@
 
 <script>
 export default {
-  name: "LoginForm"
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    submitForm() {
+
+    }
+  }
 };
 </script>
 
