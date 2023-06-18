e8da9b4a92cb1c244f6bd5c00633f6d354e0ed2b
hmmini <pak4184@naver.com>
2023-06-18 16:38:01 +0900

    - Cors 에러 해결

--- src/api/common/interceptors.js
+++ src/api/common/interceptors.js
@@ -4,8 +4,7 @@ export function setInterceptors(instance) {
   // Add a request interceptor
   instance.interceptors.request.use(
     function(config) {
-    config.headers.Authorization = store.state.accessToken;
-    console.log(config);
+    config.headers.Authorization = store.state.accessToken ? 'Bearer ' + store.state.accessToken : '';
     return config;
   }, function(error) {
     return Promise.reject(error);
