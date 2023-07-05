cadad7ed0f0a515ce51cc67621d373f49c5c1d34
hmmini <pak4184@naver.com>
2023-07-05 20:22:52 +0900

    - 삭제 api 연결

--- src/api/posts.js
+++ src/api/posts.js
@@ -8,4 +8,8 @@ function createPost(postData) {
   return posts.post('/', postData);
 }
 
-export { fetchPosts, createPost }
+function deletePost(postData) {
+  return posts.delete('/', postData);
+}
+
+export { fetchPosts, createPost, deletePost }
--- src/components/posts/PostListItem.vue
+++ src/components/posts/PostListItem.vue
@@ -4,16 +4,19 @@
     <div class="post-contents">{{ postItem.contents }}</div>
     <div class="post-time">{{ postItem.createdDate }}
       <i class="icon ion-md-create">
-        <ion-icon name="create-outline"></ion-icon>
+        <ion-icon name="create-outline" size="small"></ion-icon>
       </i>
       <i class="icon ion-md-create">
-        <ion-icon name="trash-outline" @click="deleteItem"></ion-icon>
+        <ion-icon name="trash-outline" size="small" @click="deleteItem"></ion-icon>
       </i>
     </div>
   </li>
 </template>
 
 <script>
+import { deletePost } from '@/api/posts';
+import VueJwtDecode from "vue-jwt-decode";
+
 export default {
   props: {
     postItem: {
@@ -22,8 +25,21 @@ export default {
     }
   },
   methods: {
-    deleteItem() {
+    async deleteItem() {
       console.log('delete');
+      const postData = {
+        id: this.postItem.id,
+        member_id: VueJwtDecode.decode(window.localStorage.getItem('accessToken').replace('Bearer ', '')).sub
+      };
+      console.log('postData', postData);
+
+      try {
+        const response = await deletePost(postData);
+        console.log('response', response);
+      }
+      catch (e) {
+        console.log('error', e);
+      }
     }
   }
 }
